b330a50360d247b96d97940025e65d3a98d575c6
CRW405 <calebwiyninger@gmail.com>
2025-04-10 22:05:53 -0500

    made our database operations and tested them. We should be set in the CRUD department.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module project2.project2 {
     requires javafx.fxml;
     requires jbcrypt;
     requires javafx.graphics;
+    requires org.junit.jupiter.api;
 
     // opens the project2.project2 package to javafx.fxml and project2.project2.controllers to javafx.fxml so that the fxml files can access the controller classes
     opens project2.project2 to javafx.fxml;
