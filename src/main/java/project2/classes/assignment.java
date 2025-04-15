8f976856716c5217afa1e9aa90db6b6717e53f3e
CRW405 <calebwiyninger@gmail.com>
2025-04-15 10:38:57 -0500

    very important commit

--- src/main/java/project2/classes/assignment.java
+++ src/main/java/project2/classes/assignment.java
@@ -1,17 +1,43 @@
 package project2.classes;
 
 public class assignment {
-    private int id;
-    private String name;
 
-    public assignment(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-    public int getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
+  private int id;
+  private String name;
+
+  public assignment(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
+/* // addressed these issues, i just want to preserve jays ramblings
+ * What needs to be done in this User class
+ *
+ * Get and Set new variables
+ * Make loose methods for later on functions
+ * integrate potential grade summaries
+ *
+ *
+ * SDKNASKDBJAFBOUFOJNDFOUND
+ * i dont understand what im supposed to be doing lowkey...
+ *
+ * okay so i make the variables and construcr
+ * I ADDED SMTTTT
+ * made a hella lot of getters and setters
+ * continued to make loose methods for later??
+ * LOOOK AT ME COMP SCI-ING?
+ * btw i hate the term loose, it sounds gross
+ * i thought about making some for the grades class but i think that would be a bit much for now
+ *
+ * what's next? idfk.
+ * maybe i start on the grades class while i let caleb do his nerd shit with the course class?
+ *
+ */
